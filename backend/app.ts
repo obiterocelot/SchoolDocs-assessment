7c279727c5ba970e4ac70bbde78c96258452fbc5
Sarah Kelly <sarahblairkelly@gmail.com>
2024-10-19 18:18:48 +1300

    fixing backendb calls

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import schoolRouter from './routes/school';
 import { sequelize } from './db'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // Sync models with database
 sequelize.sync().then(() => console.log('db is ready!'));
